5c442a023856352b835acf0753d68aea321e7f35
Scott Gibson <scottgibson21@gmail.com>
2020-05-26 13:53:35 -0700

    feat: add base about page setup

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,67 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import { FullscreenExit } from "@material-ui/icons";
 
 function About() {
-  return <div>The About Page</div>;
+  return (
+    <Container style={styles.container} fluid="md">
+      <Row>
+        <Col style={styles.aboutHeader}>
+          <span>About Investio</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col style={styles.aboutPic}>
+          <Image src={require("../assets/cat-pic.jpg")} roundedCircle />
+        </Col>
+      </Row>
+      <Row>
+        <Col style={styles.aboutBody}>
+          <div style={styles.aboutBodyDiv}>
+            <p>
+              {" "}
+              Hi I'm Scott, Im a software engineer with a passion for personal
+              finance and investing. I am a firm believer that investing
+              knowledge should be freely available for all.
+            </p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
+
+const styles = {
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  aboutHeader: {
+    marginTop: 75,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 150,
+    fontFamily: "Permanent Marker",
+    fontSize: 75,
+  },
+  aboutPic: {
+    height: 250,
+    textAlign: "center",
+  },
+  aboutBody: {
+    display: "flex",
+    justifyContent: "center",
+    height: 400,
+  },
+  aboutBodyDiv: {
+    width: 500,
+    fontFamily: "Roboto",
+    textAlign: "center",
+  },
+};
 
 export default About;
