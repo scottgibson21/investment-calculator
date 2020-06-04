0a6e75f812a77315c21cdd19663f065d759f497a
Scott Gibson <scottgibson21@gmail.com>
2020-06-04 15:14:48 -0700

    feat: make about page responsive

--- src/components/About.js
+++ src/components/About.js
@@ -6,68 +6,69 @@ import Image from "react-bootstrap/Image";
 
 function About() {
   return (
-    <Container style={styles.container} fluid="md">
-      <Row>
-        <Col style={styles.aboutHeader}>
+    <Row style={styles.mainRow}>
+      <Col style={styles.column} xs={12} sm={8} lg={4}>
+        <div style={styles.aboutHeader}>
           <span>About Nvestio</span>
-        </Col>
-      </Row>
-      <Row>
-        <Col style={styles.aboutPic}>
+        </div>
+        <div style={styles.aboutPic}>
           <Image src={require("../assets/cat-pic.jpg")} roundedCircle />
-        </Col>
-      </Row>
-      <Row>
-        <Col style={styles.aboutBody}>
-          <div style={styles.aboutBodyDiv}>
-            <p>
-              Hi I'm Scott. I’m a software engineer with a passion for personal
-              finance and investing. I created Nvestio as a tool for
-              illustrating the power of compounding interest and spreading
-              awareness and knowledge about personal investing.
-            </p>
-            <p style={styles.signature}>
-              <br />
-              Scott
-            </p>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+        </div>
+        <div style={styles.aboutBodyDiv}>
+          <p>
+            Hi I'm Scott. I’m a software engineer with a passion for personal
+            finance and investing. I created Nvestio as a tool for illustrating
+            the power of compounding interest and spreading awareness and
+            knowledge about personal investing.
+          </p>
+          <p style={styles.signature}>
+            <br />
+            Scott
+          </p>
+        </div>
+      </Col>
+    </Row>
   );
 }
 
 const styles = {
-  container: {
+  mainRow: {
     justifyContent: "center",
     alignItems: "center",
-    border: "none",
-    width: 700,
     color: "#FFFFFF",
+    margin: 0,
   },
   aboutHeader: {
-    marginTop: 75,
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    height: 150,
     fontFamily: "Permanent Marker",
     fontSize: 75,
+    textAlign: "center",
+  },
+  column: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#BDC3C7",
+    borderRadius: "15px",
+    color: "black",
+    margin: 15,
   },
   aboutPic: {
-    height: 250,
     textAlign: "center",
   },
   aboutBody: {
     display: "flex",
     justifyContent: "center",
-    height: 400,
   },
   aboutBodyDiv: {
-    width: 500,
     fontFamily: "Roboto",
     textAlign: "center",
     fontSize: 18,
+    marginTop: 30,
+    paddingRight: 50,
+    paddingLeft: 50,
   },
   signature: {
     textAlign: "left",
